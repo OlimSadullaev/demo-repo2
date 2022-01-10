42e403e5d89b27814b4f9bfef3b53f686a9916c5
Olim <olimxonnutfullaevich@gmail.com>
2022-01-10 21:19:21 +0500

    hello world

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace demo_repo2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
