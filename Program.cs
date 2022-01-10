189a8aa0ee4da098984caec37def588634f32946
Olim <olimxonnutfullaevich@gmail.com>
2022-01-10 21:16:11 +0500

    messsage

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace demo_repo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
